b17884543b74694534a25e7345b64da20c283f38
Andrew Wagner <38209339+andrewwagnerkg@users.noreply.github.com>
2020-03-07 23:11:36 +0600

    Add classes and interfacess
    Add Account, UpRecord classes and IUpper interface

---
+++ DieselElcatKgUpper/DieselElcatKgUpper/Classes/IUpper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieselElcatKgUpper.Classes
+{
+    public interface IUpper
+    {
+        void Login(string login, string password);
+        void Logout();
+        void Up(int themeId);
+    }
+}
